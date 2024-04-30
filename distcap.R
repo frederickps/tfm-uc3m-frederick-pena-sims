6b9c0598b2521ad4757e0026172de247068fcb43
frederickps <frederick.penasims@web.de>
2024-04-30 17:27:23 +0200

    finished distance to prov cap

---
+++ distcap.R
@@ -0,0 +1,47 @@
+# setwd("~/Documents/Projects/unu-wider")
+options(stringsAsFactors = FALSE)
+# List of packages
+pkg = c("dplyr", "sf","sp", "stringr")
+# Checks if they are installed, install if not
+if (length(setdiff(pkg, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(pkg, rownames(installed.packages())))}
+# Load
+invisible(lapply(pkg, library, character.only = TRUE))
+
+
+
+## DATA ------------------------------------
+
+# Load capitales provincia
+caps = read.csv("Data/provcapitals.csv") %>% 
+  select(prov_code, lat, long) %>% 
+  st_as_sf(coords = c("long", "lat"), crs = 4326) %>%
+  st_transform(crs = 32630)
+
+
+shp <- st_read("Data/spatial/recintos_municipales_inspire_peninbal_etrs89.shp")
+
+# Variable names
+shp$INE <- str_sub(shp$NATCODE, start = -5)
+shp$prov_code = as.integer(str_sub(shp$INE, -5L, -4L))
+shp$dist_prov_cap = NA
+shp$area_sqkm <- as.numeric(st_area(st_geometry(shp$geometry)))/1000000
+
+# Project and reproject to UTM
+shp <- st_transform(shp, crs = 32630)
+
+# Loop for each province
+for(p in unique(caps$prov_code)){
+  shpprov <- shp[shp$prov_code == p, ]
+  centr <- st_centroid(shpprov)
+  points <- caps[caps$prov_code == p, ]
+  distances <- st_distance(centr, points) / 1000
+  shp$dist_prov_cap[shp$prov_code == p] <- distances
+}
+
+
+
+dist_cap = shp %>% select(INE, dist_prov_cap, area_sqkm) %>%
+  st_drop_geometry()
+write.csv(dist_cap, "Data/distcap.csv", row.names = FALSE)
+
